0750a027a02035138dcc31f72bd62074681aa5fc
Nikki <nikki.satopdx@gmail.com>
2020-09-23 12:04:03 -0700

    created userlist

---
+++ src/user/components/UserItem.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './UserItem.css';
+
+const UserItem = () => {
+  return <h2>Users Item!</h2>;
+};
+
+export default UserItem;
--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -1,7 +1,18 @@
 import React from 'react';
+import UsersList from '../components/UsersList';
 
 const Users = () => {
-  return <h2>Users works!</h2>;
+  const USERS = [
+    {
+      id: 'u1',
+      name: 'Nikki Sato',
+      image:
+        'https://www.google.com/url?sa=i&url=https%3A%2F%2Fdogtime.com%2Fdog-breeds%2Fpembroke-welsh-corgi&psig=AOvVaw0yX6urqbWGzORnxTouAe2Z&ust=1600974149829000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCJDF0M77_-sCFQAAAAAdAAAAABAF',
+      places: 3,
+    },
+  ];
+
+  return <UsersList items={USERS} />;
 };
 
 export default Users;
